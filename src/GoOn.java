28181ad4f1ce482671d18fa9b027552201c664d8
HikmetY <hikmetyurtbakan@yahoo.com>
2020-11-10 22:17:49 -0600

    With comment and a print statement we will publish.

--- src/GoOn.java
+++ src/GoOn.java
@@ -1,6 +1,6 @@
 public class GoOn {
     public static void main(String[] args) {
         //First time main method was created by itself. After that it will be created by user.
-
+        System.out.println("This is a practice, really good practice");
     }
 }
