4b21361b750cea23dd7ef6053af3b5fc0e9c1f6e
HikmetY <hikmetyurtbakan@yahoo.com>
2020-11-10 22:10:23 -0600

    gitignore folder was created. I added to 2nd commit.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("This is brunch issue");
+        //I try to create .gitignore folder
 
     }
 }
