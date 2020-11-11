7ef51cd3a7051eb985e42c031f24e21ba0bdfcc0
HikmetY <hikmetyurtbakan@yahoo.com>
2020-11-10 22:32:36 -0600

    I added some comment last of the code.

--- src/NoLast.java
+++ src/NoLast.java
@@ -2,5 +2,6 @@ public class NoLast {
     public static void main(String[] args) {
         System.out.println("I didn't publish yet.");
         //I tried to push all these Classes.
+        //Really I know what I do.
     }
 }
